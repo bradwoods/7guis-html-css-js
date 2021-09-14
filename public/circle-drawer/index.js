0a9d583ffe682b3a26b8f2bfee388553a4cbcade
Brad Woods <brad@easyagile.com>
2021-09-14 19:45:53 +1000

    Complete circle drawer

--- public/circle-drawer/index.js
+++ public/circle-drawer/index.js
@@ -1,23 +1,15 @@
-/* assumptions
-new circle diameter is 24px
-diameter should change in steps of 1px
-min diameter is 8, max diameter is 100
-
-// disable creating circle when menu or form is open
-
-disable undo if no changes
-disable redo if nothing to redo
-*/
-
-// TODO - add states to prevent creating circles when menu's open
-
 let selected = null;
+let changesUndone = [];
 
 const DEFAULT_DIAMETER = 24;
 const SVG_NAME_SPACE = "http://www.w3.org/2000/svg";
 
+const classes = {
+  disabled: "disabled",
+  selected: "selected",
+};
+
 const changes = [];
-let changesUndone = [];
 
 const changeTypes = {
   circleAdded: "circleAdded",
@@ -25,23 +17,21 @@ const changeTypes = {
 };
 
 const elems = {
-  undoButton: document.querySelector("button#undo"),
-  redoButton: document.querySelector("button#redo"),
   form: document.querySelector("form#adjustDiameter"),
-  label: document.querySelector("label"),
-  diameterInput: document.querySelector("input#diameter"),
-  svg: document.querySelector("svg"),
+  input: document.querySelector("input#diameter"),
+  label: document.querySelector("label[for='diameter']"),
   menu: document.querySelector("section#menu"),
   redo: document.querySelector("button#redo"),
+  svg: document.querySelector("svg"),
   undo: document.querySelector("button#undo"),
 };
 
 function hide(elem) {
-  elem.style.display = "none";
+  elem.classList.add("hide");
 }
 
 function show(elem) {
-  elem.style.display = "flex";
+  elem.classList.remove("hide");
 }
 
 function enable(elem) {
@@ -53,24 +43,37 @@ function disable(elem) {
 }
 
 function select(circle) {
-  circle.setAttribute("fill", "gray");
+  circle.classList.add(classes.selected);
   selected = {
     circle,
     diameter: circle.getAttribute("r") * 2,
   };
 }
 
+function deselect() {
+  selected.circle.classList.remove(classes.selected);
+  selected = null;
+}
+
 function loadForm() {
   const x = selected.circle.getAttribute("cx");
   const y = selected.circle.getAttribute("cy");
 
-  elems.diameterInput.value = selected.diameter;
+  elems.input.value = selected.diameter;
   elems.label.innerHTML = `Adjust diameter of circle at (${x}, ${y}).`;
 
   show(elems.form);
 }
 
-// undo / redo ------------------------------------------
+function disableSvg() {
+  elems.svg.classList.add(classes.disabled);
+}
+
+function enableSvg() {
+  elems.svg.classList.remove(classes.disabled);
+}
+
+// Undo / redo ------------------------------------------
 function undoDiameterChange(change) {
   const { oldDiameter } = change;
   change.circle.setAttribute("r", oldDiameter / 2);
@@ -79,6 +82,161 @@ function undoDiameterChange(change) {
 function redoDiameterChange(change) {
   const { newDiameter } = change;
   change.circle.setAttribute("r", newDiameter / 2);
+}
+
+function resetChangesUndone() {
+  changesUndone = [];
+  disable(elems.redo);
+}
+
+// Logging ----------------------------------------------------------
+function logChange(change) {
+  changes.push(change);
+  enable(elems.undo);
+}
+
+function logCircleAdd(circle) {
+  logChange({
+    type: changeTypes.circleAdded,
+    circle,
+  });
+}
+
+function logDiameterChange() {
+  const newDiameter = selected.circle.getAttribute("r") * 2;
+  const oldDiameter = selected.diameter;
+
+  if (newDiameter !== oldDiameter) {
+    logChange({
+      type: changeTypes.diameterChange,
+      circle: selected.circle,
+      oldDiameter,
+      newDiameter,
+    });
+
+    resetChangesUndone();
+  }
+}
+
+// Circle ------------------------------------------------------------
+function mouseToSvgCoords(event) {
+  const invertedSVGMatrix = elems.svg.getScreenCTM().inverse();
+  const point = elems.svg.createSVGPoint();
+
+  point.x = event.clientX;
+  point.y = event.clientY;
+
+  return point.matrixTransform(invertedSVGMatrix);
+}
+
+function removeCircle(circle) {
+  circle.removeEventListener("click", onCircleClick);
+  circle.removeEventListener("contextmenu", onCircleRightClick);
+
+  elems.svg.removeChild(circle);
+}
+
+function addCircle(circle) {
+  circle.addEventListener("click", onCircleClick);
+  circle.addEventListener("contextmenu", onCircleRightClick);
+
+  elems.svg.appendChild(circle);
+}
+
+function createCircle(coords) {
+  const { x, y } = coords;
+  const circle = document.createElementNS(SVG_NAME_SPACE, "circle");
+
+  circle.setAttribute("r", DEFAULT_DIAMETER);
+  circle.setAttribute("cx", x);
+  circle.setAttribute("cy", y);
+  circle.setAttribute("fill", "transparent");
+  circle.setAttribute("stroke", "black");
+
+  return circle;
+}
+
+// State ---------------------------------------------------------
+const states = {
+  default: "default",
+  menuOpen: "menuOpen",
+  formOpen: "formOpen",
+};
+
+const state = {
+  set change(value) {
+    this.value = value;
+    onStateChange(value);
+  },
+  value: states.default,
+};
+
+function onStateChange(state) {
+  switch (state) {
+    case states.default:
+      if (changes.length > 0) {
+        enable(elems.undo);
+      }
+      if (changesUndone.length > 0) {
+        enable(elems.redo);
+      }
+
+      enableSvg();
+      deselect();
+      hide(elems.menu);
+      hide(elems.form);
+      break;
+
+    case states.menuOpen:
+      show(elems.menu);
+      disable(elems.undo);
+      disable(elems.redo);
+      disableSvg();
+      break;
+
+    default:
+      throw Error(`Unknown state: ${state}`);
+  }
+}
+
+// Event listeners ---------------------------------------------------------
+function onSvgClick(event) {
+  const coords = mouseToSvgCoords(event);
+
+  const circle = createCircle(coords);
+  addCircle(circle);
+  logCircleAdd(circle);
+  resetChangesUndone();
+}
+
+function onCircleClick(event) {
+  event.stopPropagation();
+}
+
+function onCircleRightClick(event) {
+  event.preventDefault();
+  select(event.target);
+  state.change = states.menuOpen;
+}
+
+function onAdjustDiameterClick() {
+  loadForm();
+  hide(elems.menu);
+  state.change = states.formOpen;
+}
+
+function onMenuCloseClick() {
+  state.change = states.default;
+}
+
+function onDiameterInput(event) {
+  const { value } = event.target;
+  selected.circle.setAttribute("r", value / 2);
+}
+
+function onFormCloseClick() {
+  logDiameterChange();
+  state.change = states.default;
 }
 
 function onUndoClick() {
@@ -131,129 +289,4 @@ function onRedoClick() {
   }
 
   enable(elems.undo);
-}
-
-function resetChangesUndone() {
-  changesUndone = [];
-  disable(elems.redo);
-}
-
-// Logging ----------------------------------------------------------
-function logChange(change) {
-  changes.push(change);
-  enable(elems.undo);
-}
-
-function logCircleAdd(circle) {
-  logChange({
-    type: changeTypes.circleAdded,
-    circle,
-  });
-}
-
-function logDiameterChange() {
-  const newDiameter = selected.circle.getAttribute("r") * 2;
-  const oldDiameter = selected.diameter;
-
-  if (newDiameter !== oldDiameter) {
-    logChange({
-      type: changeTypes.diameterChange,
-      circle: selected.circle,
-      oldDiameter,
-      newDiameter,
-    });
-
-    resetChangesUndone();
-  }
-}
-
-// Circle ------------------------------------------------------------
-function mouseToSvgCoords(event) {
-  const invertedSVGMatrix = elems.svg.getScreenCTM().inverse();
-  const point = elems.svg.createSVGPoint();
-
-  point.x = event.clientX;
-  point.y = event.clientY;
-
-  return point.matrixTransform(invertedSVGMatrix);
-}
-
-function removeCircle(circle) {
-  circle.removeEventListener("click", onCircleClick);
-  circle.removeEventListener("mouseover", onCircleMouseOver);
-  circle.removeEventListener("mouseout", onCircleMouseOut);
-  circle.removeEventListener("contextmenu", onCircleRightClick);
-
-  elems.svg.removeChild(circle);
-}
-
-function addCircle(circle) {
-  circle.addEventListener("click", onCircleClick);
-  circle.addEventListener("mouseover", onCircleMouseOver);
-  circle.addEventListener("mouseout", onCircleMouseOut);
-  circle.addEventListener("contextmenu", onCircleRightClick);
-
-  elems.svg.appendChild(circle);
-}
-
-function createCircle(coords) {
-  const { x, y } = coords;
-  const id = `${x}${y}`;
-  const circle = document.createElementNS(SVG_NAME_SPACE, "circle");
-
-  circle.setAttribute("id", id);
-  circle.setAttribute("r", DEFAULT_DIAMETER);
-  circle.setAttribute("cx", x);
-  circle.setAttribute("cy", y);
-  circle.setAttribute("fill", "transparent");
-  circle.setAttribute("stroke", "black");
-
-  return circle;
-}
-
-// event listeners ---------------------------------------------------------
-function onSvgLeftClick(event) {
-  const coords = mouseToSvgCoords(event);
-
-  const circle = createCircle(coords);
-  addCircle(circle);
-  logCircleAdd(circle);
-  resetChangesUndone();
-}
-
-function onCircleMouseOver(event) {
-  select(event.target);
-}
-
-function onCircleMouseOut(event) {
-  // we don't remove the value for 'selected' here in case the user is attempting to change the circle's diameter
-  event.target.setAttribute("fill", "transparent");
-}
-
-function onCircleClick(event) {
-  event.stopPropagation();
-}
-
-function onCircleRightClick(event) {
-  event.preventDefault();
-  show(elems.menu);
-}
-
-function onAdjustDiameterClick() {
-  loadForm();
-  hide(elems.menu);
-}
-
-function onMenuCloseClick() {
-  hide(elems.menu);
-}
-
-function onDiameterInput(event) {
-  const { value } = event.target;
-  selected.circle.setAttribute("r", value / 2);
-}
-
-function onFormCloseClick() {
-  hide(elems.form);
-  logDiameterChange();
 }
